56a6ca94ac21a089628d4e2c8d6220956ee4414c
Jens Roggeband <jensroggeband@gmail.com>
2022-02-22 16:32:51 +0100

    Clean up

--- app/src/main/java/com/jensroggeband/coffeeit/model/Selection.kt
+++ app/src/main/java/com/jensroggeband/coffeeit/model/Selection.kt
@@ -2,7 +2,6 @@ package com.jensroggeband.coffeeit.model
 
 abstract class Selection(
     val name: String,
-//    val icon: Int,
     val selectedOption: Selection? = null,
     val subSelections: List<Selection> = emptyList()
 )
